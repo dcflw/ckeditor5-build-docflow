b98ef95cf4a4a433ce878b8dc16a83f3b4f437da
Martijn Pieters <mail@martijnpieters.nl>
2020-04-03 10:28:14 +0200

    T2542 Transform spread operator

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
     ],
   },
 
