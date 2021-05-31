cefbc75d720efc46aa203864ab8324327df1bce2
Sebastian Tomescu <sebastian.tomescu@gmail.com>
2021-05-31 15:23:41 +0200

    Add internal link functionality

--- src/plugins/docflow-link/docflow-link-editing.js
+++ src/plugins/docflow-link/docflow-link-editing.js
@@ -1,7 +1,137 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import {
+	toWidget,
+	viewToModelPositionOutsideModelElement,
+} from "@ckeditor/ckeditor5-widget/src/utils";
+import Widget from "@ckeditor/ckeditor5-widget/src/widget";
+
+import DocflowLinkCommand, {
+	COMMAND_INTERNAL_LINK,
+} from "./docflow-link-command";
+
+export const ATTRIBUTE_ID = "id";
+export const ATTRIBUTE_NAME = "name";
+export const ATTRIBUTE_REFERENCE = "reference";
+export const CONFIG_NAMESPACE = "docflowLink";
+export const CUSTOM_PROPERTY_ID = "id";
+export const CUSTOM_PROPERTY_NAME = "name";
+export const CUSTOM_PROPERTY_REFERENCE = "reference";
+export const CUSTOM_PROPERTY_TYPE = "type";
+export const TYPE_INTERNAL_LINK = "internalLink"
 
 export default class DocflowLinkEditing extends Plugin {
+	static get requires() {
+		return [Widget];
+	}
+
 	init() {
-		console.log('DocflowLinkEditing#init() got called');
+		this.defineSchema();
+		this.defineConverters();
+
+		this.editor.commands.add(
+			COMMAND_INTERNAL_LINK,
+			new DocflowLinkCommand(this.editor),
+		);
+
+		this.editor.editing.mapper.on(
+			"viewToModelPosition",
+			viewToModelPositionOutsideModelElement(this.editor.model, viewElement =>
+				viewElement.hasClass(TYPE_INTERNAL_LINK),
+			),
+		);
+		this.editor.config.define(CONFIG_NAMESPACE, {
+			formComponent: undefined,
+			formComponentProps: {},
+		});
+	}
+
+	defineSchema() {
+		const schema = this.editor.model.schema;
+
+		schema.register(TYPE_INTERNAL_LINK, {
+			allowIn: "tableCell",
+			allowWhere: "$text",
+			isInline: true,
+			isObject: true,
+			allowAttributes: [ATTRIBUTE_ID, ATTRIBUTE_NAME, ATTRIBUTE_REFERENCE],
+		});
+	}
+
+	defineConverters() {
+		const conversion = this.editor.conversion;
+
+		// view-to-model converter
+		conversion
+			.for("upcast")
+			.elementToElement({
+				view: {
+					name: "span",
+					attributes: {
+						"data-docflow-type": TYPE_INTERNAL_LINK,
+					},
+				},
+				model: (viewElement, modelWriter) => {
+					const id = viewElement.getAttribute("data-reference-id");
+					const reference = viewElement.getAttribute("data-reference-type");
+					let name = "";
+
+					if (viewElement.childCount === 1) {
+						name = viewElement.getChild(0).data;
+					}
+
+					return modelWriter.createElement(TYPE_INTERNAL_LINK, { id, reference, name });
+				},
+			});
+
+		// model-to-view converter (editor)
+		conversion
+			.for("editingDowncast")
+			.elementToElement({
+				model: TYPE_INTERNAL_LINK,
+				view: (modelItem, viewWriter) => {
+					const widgetElement = this.createViewInternalLink(
+						modelItem,
+						viewWriter,
+						true,
+					);
+
+					return toWidget(widgetElement, viewWriter);
+				},
+			});
+
+		// model-to-view converter (data)
+		conversion
+			.for("dataDowncast")
+			.elementToElement({
+				model: TYPE_INTERNAL_LINK,
+				view: this.createViewInternalLink,
+			});
+	}
+
+	createViewInternalLink(modelItem, viewWriter, editorView = false) {
+		const name = modelItem.getAttribute("name");
+		const id = modelItem.getAttribute("id");
+		const reference = modelItem.getAttribute("reference");
+
+		const attributes = {
+			"data-docflow-type": TYPE_INTERNAL_LINK,
+			"data-reference-id": id,
+			"data-reference-type": reference,
+		};
+
+		if (editorView) {
+			attributes["class"] = "internalLink";
+		}
+
+		const view = viewWriter.createContainerElement("span", attributes);
+		const innerText = viewWriter.createText(name);
+
+		viewWriter.insert(viewWriter.createPositionAt(view, 0), innerText);
+		viewWriter.setCustomProperty(CUSTOM_PROPERTY_TYPE, TYPE_INTERNAL_LINK, view);
+		viewWriter.setCustomProperty(CUSTOM_PROPERTY_NAME, name, view);
+		viewWriter.setCustomProperty(CUSTOM_PROPERTY_ID, id, view);
+		viewWriter.setCustomProperty(CUSTOM_PROPERTY_REFERENCE, reference, view);
+
+		return view;
 	}
 }
--- src/plugins/docflow-link/docflow-link-ui.js
+++ src/plugins/docflow-link/docflow-link-ui.js
@@ -1,14 +1,39 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import clickOutsideHandler from "@ckeditor/ckeditor5-ui/src/bindings/clickoutsidehandler";
 import Model from '@ckeditor/ckeditor5-ui/src/model';
 import Collection from '@ckeditor/ckeditor5-utils/src/collection';
 import { addListToDropdown, createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import ContextualBalloon from "@ckeditor/ckeditor5-ui/src/panel/balloon/contextualballoon";
+
+import ReactView from "../../views/react-view";
+
 import linkIcon from "./theme/icons/link-icon.svg";
+
+import { CONFIG_NAMESPACE, CUSTOM_PROPERTY_ID, CUSTOM_PROPERTY_REFERENCE, TYPE_INTERNAL_LINK } from "./docflow-link-editing"
+import { COMMAND_INTERNAL_LINK } from "./docflow-link-command";
 
 export default class DocflowLinkUI extends Plugin {
 	init() {
 		const editor = this.editor;
-		console.log(editor);
-		this.addToolbarButton();
+
+		if (editor.config.get(`${CONFIG_NAMESPACE}.enabled`)) {
+			const config = editor.config._config;
+			const formComponent =
+			  config[CONFIG_NAMESPACE] && config[CONFIG_NAMESPACE].formComponent;
+			const formComponentProps = editor.config.get(
+			  `${CONFIG_NAMESPACE}.formComponentProps`,
+			);
+
+			this.balloon = editor.plugins.get(ContextualBalloon);
+			this.reactView = new ReactView(editor.locale, formComponent, {
+				...formComponentProps,
+				onInsertInternalLink: this.handleInsertInternalLink.bind(this),
+			});
+
+			this.addToolbarButton();
+			this.enableUserBalloonInteractions();
+		}
+
 	}
 
 	addToolbarButton() {
@@ -20,7 +45,7 @@ export default class DocflowLinkUI extends Plugin {
 				icon: linkIcon,
 				tooltip: true,
 			});
-			
+
 			const items = new Collection();
 
 			items.add({
@@ -41,12 +66,11 @@ export default class DocflowLinkUI extends Plugin {
 			});
 
 			addListToDropdown(dropdownView, items);
-			//this.listenTo(button, "execute", () => this.showBalloon());
 
 			this.listenTo(dropdownView, 'execute', evt => {
 				const { id } = evt.source;
 				if (id === 'internal-link') {
-					console.log("internal link");
+					this.showBalloon();
 				} else if (id === 'external-link') {
 					const externalLinkPlugin = this.editor.plugins.get("LinkUI");
 					externalLinkPlugin._showUI(true);
@@ -56,4 +80,131 @@ export default class DocflowLinkUI extends Plugin {
 			return dropdownView;
 		});
 	}
+
+	enableUserBalloonInteractions() {
+		const hideBalloonIfNotSelected = () => {
+			if (this.isBalloonVisible() && !this.getSelectedInternalLinkElement()) {
+				this.hideBalloon();
+			}
+		};
+
+		this.editor.keystrokes.set("esc", (data, cancel) => {
+			if (this.isBalloonVisible()) {
+				this.hideBalloon();
+				cancel();
+			}
+		});
+
+		this.editor.editing.view.document.on("click", () => {
+			const element = this.getSelectedInternalLinkElement();
+
+			if (element) {
+				this.showBalloon(element);
+			}
+		});
+
+		this.editor.editing.view.document.selection.on("change", () =>
+			setTimeout(hideBalloonIfNotSelected),
+		);
+
+		clickOutsideHandler({
+			emitter: this.reactView,
+			activator: () => this.isBalloonVisible(),
+			contextElements: [this.balloon.view.element],
+			callback: () => this.hideBalloon(),
+		});
+	}
+
+	handleInsertInternalLink(id, reference, name) {
+    this.editor.execute(COMMAND_INTERNAL_LINK, { id, reference, name });
+  }
+
+	showBalloon(element) {
+		let props = {
+		  selectedId: undefined,
+			selectedReference: undefined,
+		};
+
+		if (element) {
+			const type = element.getCustomProperty("type");
+
+			if (type === TYPE_INTERNAL_LINK) {
+			  props.selectedId = element.getCustomProperty(
+			    CUSTOM_PROPERTY_ID,
+			  );
+				props.selectedReference = element.getCustomProperty(
+			    CUSTOM_PROPERTY_REFERENCE,
+			  );
+			} 
+		}
+
+		this.reactView.updateProps(props);
+
+		if (this.balloon.hasView(this.reactView)) {
+			this.balloon.remove(this.reactView);
+		}
+
+		this.balloon.add({
+			singleViewModel: true,
+			view: this.reactView,
+			position: this.getBalloonPositionData(),
+		})
+
+		this.removeClassCkResetAll();
+		this.balloon.updatePosition();
+	}
+
+	hideBalloon() {
+		if (this.isBalloonVisible()) {
+			this.balloon.remove(this.reactView);
+			this.addClassCkResetAll();
+		}
+	}
+
+	isBalloonVisible() {
+		return this.balloon.visibleView === this.reactView;
+	}
+
+	getSelectedInternalLinkElement() {
+		const view = this.editor.editing.view;
+		const selection = view.document.selection;
+		const selectedElement = selection.getSelectedElement();
+
+		if (selectedElement) {
+			const type = selectedElement.getCustomProperty("type");
+
+			if (type === TYPE_INTERNAL_LINK) {
+				return selectedElement;
+			}
+		}
+
+		return null;
+	}
+
+	getBalloonPositionData() {
+		const view = this.editor.editing.view;
+		const viewDocument = view.document;
+		const target = view.domConverter.viewRangeToDom(
+			viewDocument.selection.getFirstRange(),
+		);
+
+		return { target };
+	}
+
+	addClassCkResetAll() {
+		const bodyContainer = this.editor.ui.view.body._parentElement;
+
+		if (bodyContainer && !bodyContainer.classList.contains("ck-reset_all")) {
+			bodyContainer.classList.add("ck-reset_all");
+		}
+	}
+
+	removeClassCkResetAll() {
+		const bodyContainer = this.editor.ui.view.body._parentElement;
+
+		if (bodyContainer && bodyContainer.classList.contains("ck-reset_all")) {
+			bodyContainer.classList.remove("ck-reset_all");
+		}
+	}
+
 }
