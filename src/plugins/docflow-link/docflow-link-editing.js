13dd929de26c7402ccb42afe036201299344935a
Sebastian Tomescu <sebastian.tomescu@gmail.com>
2021-06-03 13:41:54 +0200

    Fix indentation

--- src/plugins/docflow-link/docflow-link-editing.js
+++ src/plugins/docflow-link/docflow-link-editing.js
@@ -1,12 +1,12 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import {
-	toWidget,
-	viewToModelPositionOutsideModelElement,
+  toWidget,
+  viewToModelPositionOutsideModelElement,
 } from "@ckeditor/ckeditor5-widget/src/utils";
 import Widget from "@ckeditor/ckeditor5-widget/src/widget";
 
 import DocflowLinkCommand, {
-	COMMAND_INTERNAL_LINK,
+  COMMAND_INTERNAL_LINK,
 } from "./docflow-link-command";
 import "./theme/docflow-link.css";
 
@@ -21,118 +21,118 @@ export const CUSTOM_PROPERTY_TYPE = "type";
 export const TYPE_INTERNAL_LINK = "InternalLink"
 
 export default class DocflowLinkEditing extends Plugin {
-	static get requires() {
-		return [Widget];
-	}
+  static get requires() {
+    return [Widget];
+  }
 
-	init() {
-		this.defineSchema();
-		this.defineConverters();
+  init() {
+    this.defineSchema();
+    this.defineConverters();
 
-		this.editor.commands.add(
-			COMMAND_INTERNAL_LINK,
-			new DocflowLinkCommand(this.editor),
-		);
+    this.editor.commands.add(
+      COMMAND_INTERNAL_LINK,
+      new DocflowLinkCommand(this.editor),
+    );
 
-		this.editor.editing.mapper.on(
-			"viewToModelPosition",
-			viewToModelPositionOutsideModelElement(this.editor.model, viewElement =>
-				viewElement.hasClass(TYPE_INTERNAL_LINK),
-			),
-		);
-		this.editor.config.define(CONFIG_NAMESPACE, {
-			formComponent: undefined,
-			formComponentProps: {},
-		});
-	}
+    this.editor.editing.mapper.on(
+      "viewToModelPosition",
+      viewToModelPositionOutsideModelElement(this.editor.model, viewElement =>
+        viewElement.hasClass(TYPE_INTERNAL_LINK),
+      ),
+    );
+    this.editor.config.define(CONFIG_NAMESPACE, {
+      formComponent: undefined,
+      formComponentProps: {},
+    });
+  }
 
-	defineSchema() {
-		const schema = this.editor.model.schema;
+  defineSchema() {
+    const schema = this.editor.model.schema;
 
-		schema.register(TYPE_INTERNAL_LINK, {
-			allowIn: "tableCell",
-			allowWhere: "$text",
-			isInline: true,
-			isObject: true,
-			allowAttributes: [ATTRIBUTE_ID, ATTRIBUTE_NAME, ATTRIBUTE_REFERENCE],
-		});
-	}
+    schema.register(TYPE_INTERNAL_LINK, {
+      allowIn: "tableCell",
+      allowWhere: "$text",
+      isInline: true,
+      isObject: true,
+      allowAttributes: [ATTRIBUTE_ID, ATTRIBUTE_NAME, ATTRIBUTE_REFERENCE],
+    });
+  }
 
-	defineConverters() {
-		const conversion = this.editor.conversion;
+  defineConverters() {
+    const conversion = this.editor.conversion;
 
-		// view-to-model converter
-		conversion
-			.for("upcast")
-			.elementToElement({
-				view: {
-					name: "span",
-					attributes: {
-						"data-docflow-type": TYPE_INTERNAL_LINK,
-					},
-				},
-				model: (viewElement, modelWriter) => {
-					const id = viewElement.getAttribute("data-reference-uuid");
-					const reference = viewElement.getAttribute("data-reference-type");
-					let name = "";
+    // view-to-model converter
+    conversion
+      .for("upcast")
+      .elementToElement({
+        view: {
+          name: "span",
+          attributes: {
+            "data-docflow-type": TYPE_INTERNAL_LINK,
+          },
+        },
+        model: (viewElement, modelWriter) => {
+          const id = viewElement.getAttribute("data-reference-uuid");
+          const reference = viewElement.getAttribute("data-reference-type");
+          let name = "";
 
-					if (viewElement.childCount === 1) {
-						name = viewElement.getChild(0).data;
-					}
+          if (viewElement.childCount === 1) {
+            name = viewElement.getChild(0).data;
+          }
 
-					return modelWriter.createElement(TYPE_INTERNAL_LINK, { id, reference, name });
-				},
-			});
+          return modelWriter.createElement(TYPE_INTERNAL_LINK, { id, reference, name });
+        },
+      });
 
-		// model-to-view converter (editor)
-		conversion
-			.for("editingDowncast")
-			.elementToElement({
-				model: TYPE_INTERNAL_LINK,
-				view: (modelItem, viewWriter) => {
-					const widgetElement = this.createViewInternalLink(
-						modelItem,
-						viewWriter,
-						true,
-					);
+    // model-to-view converter (editor)
+    conversion
+      .for("editingDowncast")
+      .elementToElement({
+        model: TYPE_INTERNAL_LINK,
+        view: (modelItem, viewWriter) => {
+          const widgetElement = this.createViewInternalLink(
+            modelItem,
+            viewWriter,
+            true,
+          );
 
-					return toWidget(widgetElement, viewWriter);
-				},
-			});
+          return toWidget(widgetElement, viewWriter);
+        },
+      });
 
-		// model-to-view converter (data)
-		conversion
-			.for("dataDowncast")
-			.elementToElement({
-				model: TYPE_INTERNAL_LINK,
-				view: this.createViewInternalLink,
-			});
-	}
+    // model-to-view converter (data)
+    conversion
+      .for("dataDowncast")
+      .elementToElement({
+        model: TYPE_INTERNAL_LINK,
+        view: this.createViewInternalLink,
+      });
+  }
 
-	createViewInternalLink(modelItem, viewWriter, editorView = false) {
-		const name = modelItem.getAttribute("name");
-		const id = modelItem.getAttribute("id");
-		const reference = modelItem.getAttribute("reference");
+  createViewInternalLink(modelItem, viewWriter, editorView = false) {
+    const name = modelItem.getAttribute("name");
+    const id = modelItem.getAttribute("id");
+    const reference = modelItem.getAttribute("reference");
 
-		const attributes = {
-			"data-docflow-type": TYPE_INTERNAL_LINK,
-			"data-reference-uuid": id,
-			"data-reference-type": reference,
-		};
+    const attributes = {
+      "data-docflow-type": TYPE_INTERNAL_LINK,
+      "data-reference-uuid": id,
+      "data-reference-type": reference,
+    };
 
-		if (editorView) {
-			attributes["class"] = "internalLink";
-		}
+    if (editorView) {
+      attributes["class"] = "internalLink";
+    }
 
-		const view = viewWriter.createContainerElement("span", attributes);
-		const innerText = viewWriter.createText(name);
+    const view = viewWriter.createContainerElement("span", attributes);
+    const innerText = viewWriter.createText(name);
 
-		viewWriter.insert(viewWriter.createPositionAt(view, 0), innerText);
-		viewWriter.setCustomProperty(CUSTOM_PROPERTY_TYPE, TYPE_INTERNAL_LINK, view);
-		viewWriter.setCustomProperty(CUSTOM_PROPERTY_NAME, name, view);
-		viewWriter.setCustomProperty(CUSTOM_PROPERTY_ID, id, view);
-		viewWriter.setCustomProperty(CUSTOM_PROPERTY_REFERENCE, reference, view);
+    viewWriter.insert(viewWriter.createPositionAt(view, 0), innerText);
+    viewWriter.setCustomProperty(CUSTOM_PROPERTY_TYPE, TYPE_INTERNAL_LINK, view);
+    viewWriter.setCustomProperty(CUSTOM_PROPERTY_NAME, name, view);
+    viewWriter.setCustomProperty(CUSTOM_PROPERTY_ID, id, view);
+    viewWriter.setCustomProperty(CUSTOM_PROPERTY_REFERENCE, reference, view);
 
-		return view;
-	}
+    return view;
+  }
 }
