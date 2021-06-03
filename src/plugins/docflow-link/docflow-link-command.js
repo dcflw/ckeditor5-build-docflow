37e0ccd567f2fae643deae8256883d249a67946a
Sebastian <sebastian.tomescu@gmail.com>
2021-06-03 13:43:52 +0200

    Add internal link plugin (#7)

---
+++ src/plugins/docflow-link/docflow-link-command.js
@@ -0,0 +1,29 @@
+import Command from "@ckeditor/ckeditor5-core/src/command";
+
+export const COMMAND_INTERNAL_LINK = "InternalLink";
+
+export default class DocflowLinkCommand extends Command {
+  execute(params) {
+    const editor = this.editor;
+
+    editor.model.change(writer => {
+      const internalLink = writer.createElement(COMMAND_INTERNAL_LINK, params);
+
+      editor.model.insertContent(internalLink);
+      writer.setSelectionFocus(internalLink, "after");
+    });
+
+    setTimeout(() => editor.editing.view.focus());
+  }
+
+  refresh() {
+    const model = this.editor.model;
+    const selection = model.document.selection;
+    const isAllowed = model.schema.checkChild(
+      selection.focus.parent,
+      "InternalLink",
+    );
+
+    this.set("isEnabled", isAllowed);
+  }
+}
---
+++ src/plugins/docflow-link/docflow-link.js
@@ -0,0 +1,10 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+import DocflowLinkEditing from './docflow-link-editing';
+import DocflowLinkUI from './docflow-link-ui';
+
+export default class DocflowLink extends Plugin {
+  static get requires() {
+    return [DocflowLinkEditing, DocflowLinkUI];
+  }
+}
