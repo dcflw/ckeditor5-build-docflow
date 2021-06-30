d56594d40218905bde05703d94c27115a7bec43e
Sebastian Tomescu <sebastian.tomescu@gmail.com>
2021-06-30 14:28:59 +0200

    Add ckeditor ruleset to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+    "extends": "ckeditor5",
 };
