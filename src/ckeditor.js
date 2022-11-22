66649a0d983ed5ccae896eaf7277d2ad0686a41a
Melih Korkmaz <melihkorkmaz84@gmail.com>
2022-11-22 11:39:54 +0100

    [#8354] Update CK Editor for adding id to html tags

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,69 +3,72 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import DecoupledEditorBase from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
-import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+import DecoupledEditorBase from "@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor";
+import InlineEditorBase from "@ckeditor/ckeditor5-editor-inline/src/inlineeditor";
 
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Font from '@ckeditor/ckeditor5-font/src/font';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableKeyboard from '@ckeditor/ckeditor5-table/src/tablekeyboard';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Font from "@ckeditor/ckeditor5-font/src/font";
+import Image from "@ckeditor/ckeditor5-image/src/image";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat";
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TableKeyboard from "@ckeditor/ckeditor5-table/src/tablekeyboard";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 
-import MultirootEditor from './editors/multiroot-editor';
-import DocflowImageUpload from './plugins/docflow-image-upload/docflow-image-upload';
-import DocflowLink from './plugins/docflow-link/docflow-link';
-import DocflowPlaceholder from './plugins/docflow-placeholder/docflow-placeholder';
-import DocflowSanitizePaste from './plugins/docflow-sanitize-paste/docflow-sanitize-paste';
-import './ckeditor.css';
+import MultirootEditor from "./editors/multiroot-editor";
+import DocflowImageUpload from "./plugins/docflow-image-upload/docflow-image-upload";
+import DocflowLink from "./plugins/docflow-link/docflow-link";
+import DocflowPlaceholder from "./plugins/docflow-placeholder/docflow-placeholder";
+import DocflowSanitizePaste from "./plugins/docflow-sanitize-paste/docflow-sanitize-paste";
+import DocflowIdGenerator from "./plugins/docflow-id-generator/docflow-id-generator";
+import "./ckeditor.css";
 
-class DecoupledEditor extends DecoupledEditorBase { }
-class InlineEditor extends InlineEditorBase { }
+class DecoupledEditor extends DecoupledEditorBase {}
+class InlineEditor extends InlineEditorBase {}
 
 const plugins = [
-	Essentials,
-	Alignment,
-	Bold,
-	DocflowImageUpload,
-	DocflowPlaceholder,
-	DocflowLink,
-	DocflowSanitizePaste,
-	Font,
-	Indent,
-	IndentBlock,
-	Image,
-	ImageStyle,
-	ImageToolbar,
-	Italic,
-	Link,
-	List,
-	ListStyle,
-	Paragraph,
-	PasteFromOffice,
-	RemoveFormat,
-	Strikethrough,
-	Table,
-	// TableCellProperties,
-	TableKeyboard,
-	// TableProperties,
-	TableToolbar,
-	Underline
+  Essentials,
+  Alignment,
+  Bold,
+  DocflowImageUpload,
+  DocflowPlaceholder,
+  DocflowLink,
+  DocflowSanitizePaste,
+  Font,
+  Indent,
+  IndentBlock,
+  Image,
+  ImageStyle,
+  ImageToolbar,
+  Italic,
+  Link,
+  List,
+  ListStyle,
+  Paragraph,
+  PasteFromOffice,
+  RemoveFormat,
+  Strikethrough,
+  Table,
+  // TableCellProperties,
+  TableKeyboard,
+  // TableProperties,
+  TableToolbar,
+  Underline,
+  // GeneralHtmlSupport,
+  DocflowIdGenerator,
 ];
 
 DecoupledEditor.builtinPlugins = plugins;
@@ -73,46 +76,46 @@ InlineEditor.builtinPlugins = plugins;
 MultirootEditor.builtinPlugins = plugins;
 
 const config = {
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en',
-	toolbar: {
-		items: [
-			'bold',
-			'italic',
-			'underline',
-			'strikethrough',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'bulletedList',
-			'numberedList',
-			'insertTable',
-			'insertPlaceholder',
-			'docflowLink',
-			'imageUpload',
-			'|',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		styles: [ 'alignLeft', 'alignRight', 'full' ],
-		toolbar: [
-			'imageStyle:alignLeft',
-			'imageStyle:full',
-			'imageStyle:alignRight'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-			// "tableProperties",
-			// "tableCellProperties",
-		]
-	}
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: "en",
+  toolbar: {
+    items: [
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "|",
+      "outdent",
+      "indent",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "insertTable",
+      "insertPlaceholder",
+      "docflowLink",
+      "imageUpload",
+      "|",
+      "undo",
+      "redo",
+    ],
+  },
+  image: {
+    styles: ["alignLeft", "alignRight", "full"],
+    toolbar: [
+      "imageStyle:alignLeft",
+      "imageStyle:full",
+      "imageStyle:alignRight",
+    ],
+  },
+  table: {
+    contentToolbar: [
+      "tableColumn",
+      "tableRow",
+      "mergeTableCells",
+      // "tableProperties",
+      // "tableCellProperties",
+    ],
+  },
 };
 
 DecoupledEditor.defaultConfig = config;
---
+++ src/plugins/docflow-id-generator/docflow-id-generator.js
@@ -0,0 +1,96 @@
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import cuid from "cuid";
+
+export default class DocflowIdGenerator extends Plugin {
+  static get pluginName() {
+    return "DocflowIdGenerator";
+  }
+
+  init() {
+    this.defineSchemes();
+    this.defineConverters();
+  }
+
+  defineSchemes() {
+    const schema = this.editor.model.schema;
+    schema.extend("$block", {
+      allowAttributes: ["id"],
+    });
+    schema.extend("tableCell", {
+      allowAttributes: ["id"],
+    });
+  }
+
+  defineConverters() {
+    const conversion = this.editor.conversion;
+
+    conversion.for("upcast").attributeToAttribute({
+      view: "id",
+      model: {
+        key: "id",
+        value: viewElement => viewElement.getAttribute("id"),
+      },
+    });
+
+    conversion.for("downcast").add(dispatcher => {
+      dispatcher.on("insert:tableRow", (evt, data, { writer, mapper }) => {
+        const tableRow = data.item;
+        const tableCells = Array.from(tableRow.getChildren());
+
+        tableCells.forEach(tableCell => {
+          writer.setAttribute(
+            "id",
+            DocflowIdGenerator.generateUniqueId(),
+            tableCell,
+          );
+          writer.setAttribute(
+            "id",
+            DocflowIdGenerator.generateUniqueId(),
+            mapper.toViewElement(tableCell),
+          );
+        });
+      });
+
+      dispatcher.on("insert:paragraph", this.insertIdAttribute);
+      dispatcher.on("insert:listItem", this.insertIdAttribute);
+      dispatcher.on("insert:imageInline", this.insertIdAttribute);
+    });
+
+    conversion.for("dataDowncast").attributeToAttribute({
+      model: {
+        name: "tableCell",
+        key: "id",
+      },
+      view: modelAttributeValue => {
+        return {
+          key: "id",
+          value: modelAttributeValue,
+        };
+      },
+    });
+  }
+
+  insertIdAttribute(_, data, { writer, mapper }) {
+    const item = data.item;
+    const parent = item.parent;
+    const prevSibling = item.previousSibling;
+    const viewElement = mapper.toViewElement(item);
+
+    let id = item.getAttribute("id");
+
+    if (parent?.name === "tableCell") {
+      return;
+    }
+
+    if (id === prevSibling?.getAttribute("id") || !id) {
+      id = DocflowIdGenerator.generateUniqueId();
+      writer.setAttribute("id", id, item);
+    }
+
+    writer.setAttribute("id", id, viewElement);
+  }
+
+  static generateUniqueId() {
+    return cuid.slug();
+  }
+}
