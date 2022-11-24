063dda4f09af713f0a4c6fc02503354c03784a58
Melih Korkmaz <melihkorkmaz84@gmail.com>
2022-11-24 10:00:18 +0100

    [#8354] Show comment span

--- src/plugins/docflow-id-generator/docflow-id-generator.js
+++ src/plugins/docflow-id-generator/docflow-id-generator.js
@@ -1,96 +1,163 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import cuid from 'cuid';
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import { viewToModelPositionOutsideModelElement } from "@ckeditor/ckeditor5-widget/src/utils";
+import cuid from "cuid";
 
 export default class DocflowIdGenerator extends Plugin {
-	static get pluginName() {
-		return 'DocflowIdGenerator';
-	}
+  static get pluginName() {
+    return "DocflowIdGenerator";
+  }
 
-	init() {
-		this.defineSchemes();
-		this.defineConverters();
-	}
+  init() {
+    this.defineSchemes();
+    this.defineConverters();
 
-	defineSchemes() {
-		const schema = this.editor.model.schema;
-		schema.extend( '$block', {
-			allowAttributes: [ 'id' ]
-		} );
-		schema.extend( 'tableCell', {
-			allowAttributes: [ 'id' ]
-		} );
-	}
+    this.editor.editing.mapper.on(
+      "viewToModelPosition",
+      viewToModelPositionOutsideModelElement(this.editor.model, viewElement =>
+        viewElement.hasAttribute("data-comment-id"),
+      ),
+    );
+  }
 
-	defineConverters() {
-		const conversion = this.editor.conversion;
+  defineSchemes() {
+    const schema = this.editor.model.schema;
+    schema.extend("$block", {
+      allowAttributes: ["id"],
+    });
+    schema.extend("tableCell", {
+      allowAttributes: ["id"],
+    });
 
-		conversion.for( 'upcast' ).attributeToAttribute( {
-			view: 'id',
-			model: {
-				key: 'id',
-				value: viewElement => viewElement.getAttribute( 'id' )
-			}
-		} );
+    schema.register("comment", {
+      allowIn: "tableCell",
+      allowWhere: "$text",
+      isInline: true,
+      isObject: true,
+      allowAttributes: ["data-comment-id"],
+    });
+  }
 
-		conversion.for( 'downcast' ).add( dispatcher => {
-			dispatcher.on( 'insert:tableRow', ( evt, data, { writer, mapper } ) => {
-				const tableRow = data.item;
-				const tableCells = Array.from( tableRow.getChildren() );
+  defineConverters() {
+    const conversion = this.editor.conversion;
 
-				tableCells.forEach( tableCell => {
-					writer.setAttribute(
-						'id',
-						DocflowIdGenerator.generateUniqueId(),
-						tableCell
-					);
-					writer.setAttribute(
-						'id',
-						DocflowIdGenerator.generateUniqueId(),
-						mapper.toViewElement( tableCell )
-					);
-				} );
-			} );
+    conversion.for("upcast").attributeToAttribute({
+      view: "id",
+      model: {
+        key: "id",
+        value: viewElement => viewElement.getAttribute("id"),
+      },
+    });
 
-			dispatcher.on( 'insert:paragraph', this.insertIdAttribute );
-			dispatcher.on( 'insert:listItem', this.insertIdAttribute );
-			dispatcher.on( 'insert:imageInline', this.insertIdAttribute );
-		} );
+    conversion.for("upcast").elementToElement({
+      view: {
+        name: "span",
+        attributes: ["data-comment-id"],
+      },
+      model: (viewElement, modelConversionApi) => {
+        const id = viewElement.getAttribute("data-comment-id");
+        let name = "";
 
-		conversion.for( 'dataDowncast' ).attributeToAttribute( {
-			model: {
-				name: 'tableCell',
-				key: 'id'
-			},
-			view: modelAttributeValue => {
-				return {
-					key: 'id',
-					value: modelAttributeValue
-				};
-			}
-		} );
-	}
+        if (viewElement.childCount === 1) {
+          name = viewElement.getChild(0).data;
+        }
+        console.log("name", name);
+        return modelConversionApi.writer.createElement("comment", {
+          name,
+          "data-comment-id": id,
+        });
+      },
+    });
 
-	insertIdAttribute( _, data, { writer, mapper } ) {
-		const item = data.item;
-		const parent = item.parent;
-		const prevSibling = item.previousSibling;
-		const viewElement = mapper.toViewElement( item );
+    conversion.for("downcast").add(dispatcher => {
+      dispatcher.on("insert:tableRow", (evt, data, { writer, mapper }) => {
+        const tableRow = data.item;
+        const tableCells = Array.from(tableRow.getChildren());
 
-		let id = item.getAttribute( 'id' );
+        tableCells.forEach(tableCell => {
+          writer.setAttribute(
+            "id",
+            DocflowIdGenerator.generateUniqueId(),
+            tableCell,
+          );
+          writer.setAttribute(
+            "id",
+            DocflowIdGenerator.generateUniqueId(),
+            mapper.toViewElement(tableCell),
+          );
+        });
+      });
 
-		if ( parent && parent.name === 'tableCell' ) {
-			return;
-		}
+      dispatcher.on("insert:paragraph", this.insertIdAttribute);
+      dispatcher.on("insert:listItem", this.insertIdAttribute);
+      dispatcher.on("insert:imageInline", this.insertIdAttribute);
+    });
 
-		if ( ( prevSibling && prevSibling.getAttribute( 'id' ) === id ) || !id ) {
-			id = DocflowIdGenerator.generateUniqueId();
-			writer.setAttribute( 'id', id, item );
-		}
+    conversion.for("dataDowncast").attributeToAttribute({
+      model: {
+        name: "tableCell",
+        key: "id",
+      },
+      view: modelAttributeValue => {
+        return {
+          key: "id",
+          value: modelAttributeValue,
+        };
+      },
+    });
 
-		writer.setAttribute( 'id', id, viewElement );
-	}
+    conversion.for("editingDowncast").elementToElement({
+      model: "comment",
+      view: (modelItem, conversionApi) => {
+        const viewWriter = conversionApi.writer;
+        const name = modelItem.getAttribute("name");
+        const id = modelItem.getAttribute("data-comment-id");
 
-	static generateUniqueId() {
-		return cuid.slug();
-	}
+        const attributes = {
+          "data-comment-id": id,
+        };
+
+        const view = viewWriter.createContainerElement("span", attributes);
+        const innerText = viewWriter.createText(name);
+
+        viewWriter.insert(viewWriter.createPositionAt(view, 0), innerText);
+        viewWriter.setCustomProperty("data-comment-id", id, view);
+        viewWriter.setCustomProperty("name", name, view);
+
+        return view;
+      },
+    });
+
+    conversion.for("dataDowncast").elementToElement({
+      model: "comment",
+      view: (modelItem, conversionApi) => {
+        const viewWriter = conversionApi.writer;
+        const name = modelItem.getAttribute("name");
+        return viewWriter.createText(name);
+      },
+    });
+  }
+
+  insertIdAttribute(_, data, { writer, mapper }) {
+    const item = data.item;
+    const parent = item.parent;
+    const prevSibling = item.previousSibling;
+    const viewElement = mapper.toViewElement(item);
+
+    let id = item.getAttribute("id");
+
+    if (parent && parent.name === "tableCell") {
+      return;
+    }
+
+    if ((prevSibling && prevSibling.getAttribute("id") === id) || !id) {
+      id = DocflowIdGenerator.generateUniqueId();
+      writer.setAttribute("id", id, item);
+    }
+
+    writer.setAttribute("id", id, viewElement);
+  }
+
+  static generateUniqueId() {
+    return cuid.slug();
+  }
 }
