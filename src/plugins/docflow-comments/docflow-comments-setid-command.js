954143d96b0e615afbdc316181e8158c99fac11b
Melih Korkmaz <melihkorkmaz84@gmail.com>
2022-11-29 14:11:03 +0100

    [#8354] Remove all comments tag

--- src/plugins/docflow-comments/docflow-comments-setid-command.js
+++ src/plugins/docflow-comments/docflow-comments-setid-command.js
@@ -1,26 +1,39 @@
 import Command from "@ckeditor/ckeditor5-core/src/command";
 
 export default class DocflowCommentsSetIdCommand extends Command {
-  findAndSetId(children, commentId, writer) {
-    for (const child of children) {
+  findCommentAttributes(root) {
+    const commentModels = [];
+
+    for (const child of root.getChildren()) {
       if (child.is("element")) {
-        this.findAndSetId(child.getChildren(), commentId, writer);
+        commentModels.push(...this.findCommentAttributes(child));
       } else {
         const attr = child.getAttribute("data-comment-id");
         if (attr === "unsaved-comment") {
-          writer.setAttribute("data-comment-id", commentId, child);
+          commentModels.push(child);
         }
       }
+    }
+
+    return commentModels;
+  }
+
+  replaceCommentModels(commentModels, commentId, writer) {
+    for (const commentModel of commentModels) {
+      commentId
+        ? writer.setAttribute("data-comment-id", commentId, commentModel)
+        : writer.removeAttribute("data-comment-id", commentModel);
     }
   }
 
   execute(params) {
     const commentId = params?.id;
+    const rootName = params?.rootName;
     const model = this.editor.model;
     model.change(writer => {
-      const root = model.document.getRoot();
-      const children = root.getChildren();
-      this.findAndSetId(children, commentId, writer);
+      const root = model.document.getRoot(rootName);
+      const commentModels = this.findCommentAttributes(root);
+      this.replaceCommentModels(commentModels, commentId, writer);
     });
   }
 }
