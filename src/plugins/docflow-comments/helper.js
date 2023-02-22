942795d11575c05a5728ea2e2b4bae31a71b1021
Melih Korkmaz <melihkorkmaz84@gmail.com>
2023-02-22 11:36:16 +0300

    String type check

--- src/plugins/docflow-comments/helper.js
+++ src/plugins/docflow-comments/helper.js
@@ -1,20 +1,28 @@
 import { MARKER_NAME } from './constants';
 
 export const getDataFromMarkerName = markerName => {
-	const [ name, commentId, leafId, selected, solved ] = markerName.split( ':' );
+	const [
+		name,
+		commentId,
+		leafId,
+		selected = '',
+		solved = ''
+	] = markerName.split( ':' );
 
-  if (name !== MARKER_NAME) {
-    return {};
-  }
+	if ( name !== MARKER_NAME ) {
+		return {};
+	}
 
 	return {
-    commentId,
-    leafId,
-    selected: selected?.toLowerCase() === 'true', 
-    solved: solved?.toLowerCase() === 'true' 
-  };
+		commentId,
+		leafId,
+		selected: selected.toLowerCase() === 'true',
+		solved: solved.toLowerCase() === 'true'
+	};
 };
 
-export const getMarkerName = ( commentId, leafId, selected, solved ) => {
-	return `${ MARKER_NAME }:${ commentId }:${ leafId }:${ selected.toString().toLowerCase() }:${ solved.toString().toLowerCase() }`;
+export const getMarkerName = ( commentId, leafId, selected = false, solved = false ) => {
+	return `${ MARKER_NAME }:${ commentId }:${ leafId }:${ selected
+		.toString()
+		.toLowerCase() }:${ solved.toString().toLowerCase() }`;
 };
