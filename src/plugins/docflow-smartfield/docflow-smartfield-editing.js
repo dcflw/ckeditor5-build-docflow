04c5955ac8905cb49d961c1ba1f3405a8c69be24
Lev Chelyadinov <leva181777@gmail.com>
2023-08-22 15:37:24 +0200

    Fix the smart field regex

--- src/plugins/docflow-smartfield/docflow-smartfield-editing.js
+++ src/plugins/docflow-smartfield/docflow-smartfield-editing.js
@@ -10,7 +10,7 @@ export const ATTRIBUTE_NAME = 'name';
 export const ATTRIBUTE_TYPE = 'type';
 export const COMMAND_INSERT_SMARTFIELD = 'insertSmartfield';
 export const COMMAND_DELETE_SMARTFIELD = 'deleteSmartfield';
-export const SMARTFIELD_REGEX = /({{[a-zA-Z\s][\s\w.]*}})/;
+export const SMARTFIELD_REGEX = /({{\s*[a-z][\s\w]*}})/i;
 
 export default class DocflowSmartfieldEditing extends Plugin {
 	static get requires() {
