35c32de9630321b725da8f662a26b94ca7be9d3e
Lev Chelyadinov <leva181777@gmail.com>
2023-11-06 12:44:44 +0100

    feat: replace the smart field icon

--- src/plugins/docfield-smartfield/docfield-smartfield-ui.js
+++ src/plugins/docfield-smartfield/docfield-smartfield-ui.js
@@ -11,7 +11,7 @@ import {
   COMMAND_INSERT_SMARTFIELD,
   SMARTFIELD_REGEX,
 } from "./docfield-smartfield-editing";
-import smartFieldRegular from "./theme/icons/smartFieldRegular.svg";
+import boltRegular from "./theme/icons/boltRegular.svg";
 import linkIcon from "./theme/icons/link-icon.svg";
 
 const TYPE = {
@@ -41,7 +41,7 @@ export default class DocfieldSmartfieldUI extends Plugin {
 
       button.set({
         label: "Smart field",
-        icon: smartFieldRegular,
+        icon: boltRegular,
         tooltip: true,
       });
 
